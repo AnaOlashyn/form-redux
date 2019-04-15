a1e5883b6b95dd76feb6d46c351607cd44babbb1
AnaOlashyn <anaolashyn@gmail.com>
2019-04-15 16:11:54 +0300

    Updates

---
+++ precache-manifest.29d6615399a42f5f1b2b28d9409ff7f1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/form-redux/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "564a085316f3ceaf9b90",
+    "url": "/form-redux/static/css/main.a0fbc83f.chunk.css"
+  },
+  {
+    "revision": "2654922119bcd0fa396b",
+    "url": "/form-redux/static/js/runtime~main.4fea8f30.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/form-redux/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "32247fcd3884b4169b88",
+    "url": "/form-redux/static/js/2.f58d1f95.chunk.js"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/form-redux/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/form-redux/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/form-redux/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/form-redux/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/form-redux/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/form-redux/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "564a085316f3ceaf9b90",
+    "url": "/form-redux/static/js/main.561b4f29.chunk.js"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/form-redux/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/form-redux/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/form-redux/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/form-redux/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/form-redux/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/form-redux/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/form-redux/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/form-redux/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "32247fcd3884b4169b88",
+    "url": "/form-redux/static/css/2.6c298df9.chunk.css"
+  },
+  {
+    "revision": "849a726a71fa0e8bb167b0f7097c7578",
+    "url": "/form-redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c51fc79e135e4c249b8c649107685358.js"
+  "/form-redux/precache-manifest.29d6615399a42f5f1b2b28d9409ff7f1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/form-redux/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
